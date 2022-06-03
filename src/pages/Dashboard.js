b0d68c7463de0d6b453be759b1c23b4fcf0a431b
Quinna Halim <quinnah@umich.edu>
2022-06-03 12:02:39 -0400

    Finished dashboard UI

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -44,12 +44,21 @@ const Dashboard = () => {
             <div className="swiper-container">
                 <div className="card-container">
                     {characters.map((character) =>
-                        <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-                            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-                                <h3>{character.name}</h3>
+                        <TinderCard 
+                            className='swipe' 
+                            key={character.name} 
+                            onSwipe={(dir) => swiped(dir, character.name)} 
+                            onCardLeftScreen={() => outOfFrame(character.name)}>
+                            <div style={{ backgroundImage: 'url(' + character.url + ')' }}
+                                className='card'>
+                            <h3>{character.name}</h3>
                             </div>
                         </TinderCard>
                     )}
+
+                    <div className="swipe-info">
+                        {lastDirection ? <p>You swiped {lastDirection}</p> : <p/> }
+                    </div>
                 </div>
             </div>
             Dashboard</div>
